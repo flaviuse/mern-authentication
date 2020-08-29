e166681b8ea100986bd7d9fc1c49b39f2eab7309
Flavien Rebattet <40322270+flaviuse@users.noreply.github.com>
2020-08-29 22:00:21 +0200

    refactor (api in client) - better exports, reduced loc

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,22 +1,23 @@
-import http from '../services/httpService';
-// AUTH
-export const postLogin = user => http.post('/auth/login', user);
+import http from "../services/httpService";
 
-export const sendResetPasswordLink = email =>
-  http.post('auth/login/forgot', { email });
+const postLogin = (user) => http.post("/auth/login", user);
+const sendResetPasswordLink = (email) => http.post("auth/login/forgot", { email });
+const resetPassword = (password, token) => http.post(`auth/login/reset/${token}`, { password });
+const postLogout = () => http.post("/auth/logout");
+const postRegister = (user) => http.post("/auth/register", user);
+const getConfirmation = (token) => http.get(`/auth/confirmation/${token}`);
+const resendConfirmation = (email) => http.post("/auth/resend", { email });
+const resetRegister = (email) => http.post("/auth/register/reset", { email });
+const getUser = () => http.get("/user");
 
-export const resetPassword = (password, token) =>
-  http.post(`auth/login/reset/${token}`, { password });
-
-export const postLogout = () => http.post('/auth/logout');
-
-export const postRegister = user => http.post('/auth/register', user);
-
-export const getConfirmation = token => http.get(`/auth/confirmation/${token}`);
-
-export const resendConfirmation = email => http.post('/auth/resend', { email });
-
-export const resetRegister = email =>
-  http.post('/auth/register/reset', { email });
-
-export const getUser = () => http.get('/user');
+export {
+  postLogin,
+  sendResetPasswordLink,
+  resetPassword,
+  postLogout,
+  postRegister,
+  getConfirmation,
+  resendConfirmation,
+  getUser,
+  resetRegister,
+};
