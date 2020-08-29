1b01584afb5fcd2c3226bc9d3277da2d48b263c1
Flavien Rebattet <40322270+flaviuse@users.noreply.github.com>
2020-08-29 21:58:44 +0200

    feat(components) - added index files for cleaner imports

---
+++ client/src/components/shared/index.js
@@ -0,0 +1,5 @@
+import Input from "./input";
+import Form from "./form";
+import ProtectedRoute from "./protectedRoute";
+
+export { Input, Form, ProtectedRoute };
---
+++ client/src/components/views/index.js
@@ -0,0 +1,21 @@
+import ConfirmPage from "./ConfirmPage.jsx";
+import Home from "./Home.jsx";
+import Profile from "./Profile.jsx";
+import NavBar from "./NavBar.jsx";
+import Login from "./Login.jsx";
+import LoginForgot from "./LoginForgot.jsx";
+import LoginResetPassword from "./LoginResetPassword.jsx";
+import Logout from "./Logout.jsx";
+import Register from "./Register.jsx";
+
+export {
+  ConfirmPage,
+  Home,
+  Profile,
+  NavBar,
+  Login,
+  LoginForgot,
+  LoginResetPassword,
+  Logout,
+  Register,
+};
