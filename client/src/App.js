58fbc98ba003c45d2b7963574b16117d0ea83b9d
Flavien Rebattet <40322270+flaviuse@users.noreply.github.com>
2020-08-29 22:13:57 +0200

    dependencies (client) - remove unused deps

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Route, Redirect, Switch } from 'react-router-dom';
-import { ConnectedRouter } from 'connected-react-router';
-import { Provider } from 'react-redux';
-import { attemptGetUser } from './store/thunks/user';
+import React, { Component } from "react";
 
-import ConfirmPage from './components/pages/ConfirmPage.jsx';
-import Home from './components/pages/Home.jsx';
-import Profile from './components/pages/Profile.jsx';
-import NavBar from './components/pages/NavBar.jsx';
-import Login from './components/pages/Login.jsx';
-import LoginForgot from './components/pages/LoginForgot.jsx';
-import LoginResetPassword from './components/pages/LoginResetPassword.jsx';
-import Logout from './components/pages/Logout.jsx';
-import Register from './components/pages/Register.jsx';
-import ProtectedRoute from './components/common/protectedRoute.jsx';
+import { connect, Provider } from "react-redux";
 
+import { Route, Redirect, Switch } from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
+
+import { attemptGetUser } from "./store/thunks/user";
+
+import {
+  ConfirmPage,
+  Home,
+  Profile,
+  NavBar,
+  Login,
+  LoginForgot,
+  LoginResetPassword,
+  Logout,
+  Register,
+} from "./components/views";
+
+import { ProtectedRoute } from "./components/shared";
 class App extends Component {
   state = {
-    loading: true
+    loading: true,
   };
 
   componentDidMount() {
@@ -27,6 +31,7 @@ class App extends Component {
       .then(() => this.setState({ loading: false }))
       .catch(() => this.setState({ loading: false }));
   }
+
   render() {
     const { store, history } = this.props;
     return (
@@ -36,23 +41,16 @@ class App extends Component {
             <React.Fragment>
               <NavBar />
               <Switch>
-                <Route path="/home" exact component={Home} />
-                <ProtectedRoute path="/my-profile" exact component={Profile} />
-                <Route
-                  path="/account/confirm/:token"
-                  exact
-                  component={ConfirmPage}
-                />
-                <Route path="/register" exact component={Register} />
-                <Route path="/login" exact component={Login} />
-                <Route path="/login/forgot" exact component={LoginForgot} />
-                <Route
-                  path="/login/reset/:token"
-                  component={LoginResetPassword}
-                />
-                <ProtectedRoute path="/logout" exact component={Logout} />
-                <Redirect from="/" exact to="/home" />
-                <Redirect to="/home" />
+                <Route path='/home' exact component={Home} />
+                <ProtectedRoute path='/my-profile' exact component={Profile} />
+                <Route path='/account/confirm/:token' exact component={ConfirmPage} />
+                <Route path='/register' exact component={Register} />
+                <Route path='/login' exact component={Login} />
+                <Route path='/login/forgot' exact component={LoginForgot} />
+                <Route path='/login/reset/:token' component={LoginResetPassword} />
+                <ProtectedRoute path='/logout' exact component={Logout} />
+                <Redirect from='/' exact to='/home' />
+                <Redirect to='/home' />
               </Switch>
             </React.Fragment>
           </ConnectedRouter>
@@ -62,13 +60,10 @@ class App extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  attemptGetUser: () => dispatch(attemptGetUser())
+const mapDispatchToProps = (dispatch) => ({
+  attemptGetUser: () => dispatch(attemptGetUser()),
 });
 
 const mapStateToProps = null;
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- client/src/components/views/Register.jsx
+++ client/src/components/views/Register.jsx
@@ -1,33 +1,27 @@
-import Joi from 'joi-browser';
-import React from 'react';
-import { connect } from 'react-redux';
+import Joi from "joi-browser";
+import React from "react";
+import { connect } from "react-redux";
 import {
   attemptRegister,
   attemptResendConfirmation,
-  attemptResetRegister
-} from '../../store/thunks/auth';
-import Form from '../common/form';
+  attemptResetRegister,
+} from "../../store/thunks/auth";
+import { Form } from "../shared";
 
 class RegisterForm extends Form {
   initialState = {
-    data: { email: '', password: '', username: '' },
+    data: { email: "", password: "", username: "" },
     errors: {},
     submited: false,
-    resend: false
+    resend: false,
   };
 
   state = this.initialState;
 
   schema = {
-    email: Joi.string()
-      .email({ minDomainAtoms: 2 })
-      .required()
-      .label('Email'),
-    password: Joi.string()
-      .min(5)
-      .required()
-      .label('Password'),
-    username: Joi.string().required()
+    email: Joi.string().email({ minDomainAtoms: 2 }).required().label("Email"),
+    password: Joi.string().min(5).required().label("Password"),
+    username: Joi.string().required(),
   };
 
   doSubmit = async () => {
@@ -35,10 +29,10 @@ class RegisterForm extends Form {
     await this.props
       .attemptRegister(data)
       .then(() => this.setState({ submited: true }))
-      .catch(error => {
+      .catch((error) => {
         if (error.response && error.response.status === 400) {
           const errors = { ...this.state.errors };
-          if (error.response.data.message.includes('Email')) {
+          if (error.response.data.message.includes("Email")) {
             errors.email = error.response.data.message;
           } else {
             errors.username = error.response.data.message;
@@ -51,16 +45,12 @@ class RegisterForm extends Form {
 
   resendEmail = async () => {
     const email = this.state.data.email;
-    await this.props
-      .attemptResendConfirmation(email)
-      .then(() => this.setState({ resend: true }));
+    await this.props.attemptResendConfirmation(email).then(() => this.setState({ resend: true }));
   };
 
   reset = async () => {
     const email = this.state.data.email;
-    await this.props
-      .attemptResetRegister(email)
-      .then(() => this.setState(this.initialState));
+    await this.props.attemptResetRegister(email).then(() => this.setState(this.initialState));
   };
 
   render() {
@@ -70,25 +60,24 @@ class RegisterForm extends Form {
           <React.Fragment>
             {!this.state.submited && (
               <div>
-                <div className="container">
+                <div className='container'>
                   <form onSubmit={this.handleSubmit}>
-                    {this.renderInput('username', 'Username', 'text')}
-                    {this.renderInput('email', 'Email', 'text')}
-                    {this.renderInput('password', 'Password', 'password')}
-                    {this.renderButton('Sign Up')}
+                    {this.renderInput("username", "Username", "text")}
+                    {this.renderInput("email", "Email", "text")}
+                    {this.renderInput("password", "Password", "password")}
+                    {this.renderButton("Sign Up")}
                   </form>
                 </div>
               </div>
             )}
             {this.state.submited && (
               <React.Fragment>
-                <div className="container">
+                <div className='container'>
                   <div>
                     <p>A verification email has been sent.</p>
                     <p>
-                      Check you mailbox : {this.state.data.email}. <br /> You
-                      have 12 hours to activate your account. It can take up to
-                      15 min to receive our email.
+                      Check you mailbox : {this.state.data.email}. <br /> You have 12 hours to
+                      activate your account. It can take up to 15 min to receive our email.
                     </p>
                     <button onClick={this.resendEmail}>
                       Did not receive the email? Click here to send again.
@@ -101,7 +90,7 @@ class RegisterForm extends Form {
         )}
         {this.state.resend && (
           <React.Fragment>
-            <div className="container">
+            <div className='container'>
               <div>Still not received an email? </div>
               Try to register again. You may have given the wrong email. <br />
               If you want to be able to user the same username :<br />
@@ -115,14 +104,10 @@ class RegisterForm extends Form {
 }
 const mapStateToProps = null;
 
-const mapDispatchToProps = dispatch => ({
-  attemptRegister: newUser => dispatch(attemptRegister(newUser)),
-  attemptResendConfirmation: email =>
-    dispatch(attemptResendConfirmation(email)),
-  attemptResetRegister: email => dispatch(attemptResetRegister(email))
+const mapDispatchToProps = (dispatch) => ({
+  attemptRegister: (newUser) => dispatch(attemptRegister(newUser)),
+  attemptResendConfirmation: (email) => dispatch(attemptResendConfirmation(email)),
+  attemptResetRegister: (email) => dispatch(attemptResetRegister(email)),
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(RegisterForm);
+export default connect(mapStateToProps, mapDispatchToProps)(RegisterForm);
