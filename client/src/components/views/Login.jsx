c59e1b858e2c708c092dd5185918ce7eaa8b9ab8
Flavien Rebattet <40322270+flaviuse@users.noreply.github.com>
2020-08-29 22:40:10 +0200

    fix(Views) - Form import path

--- client/src/components/views/Login.jsx
+++ client/src/components/views/Login.jsx
@@ -1,24 +1,28 @@
-import Joi from "joi-browser";
-import React from "react";
-import { connect } from "react-redux";
-import { Link, Redirect } from "react-router-dom";
-import { attemptLogin } from "../../store/thunks/auth";
-import { Form } from "../shared";
+import Joi from 'joi-browser';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import { attemptLogin } from '../../store/thunks/auth';
+import Form from '../common/form.jsx';
 
 class LoginForm extends Form {
   state = {
-    data: { username: "", password: "" },
-    errors: {},
+    data: { username: '', password: '' },
+    errors: {}
   };
 
   schema = {
-    username: Joi.string().label("Username").required(),
-    password: Joi.string().label("Password").required(),
+    username: Joi.string()
+      .label('Username')
+      .required(),
+    password: Joi.string()
+      .label('Password')
+      .required()
   };
 
   doSubmit = async () => {
     const { data } = this.state;
-    await this.props.attemptLogin(data).catch((error) => {
+    await this.props.attemptLogin(data).catch(error => {
       if (error.response && error.response.status === 400) {
         const errors = { ...this.state.errors };
         errors.username = error.response.data.message;
@@ -28,20 +32,20 @@ class LoginForm extends Form {
   };
 
   render() {
-    if (this.props.isAuth) return <Redirect to='/home' />; // ne peut pas se relog si deja log
+    if (this.props.isAuth) return <Redirect to="/home" />; // ne peut pas se relog si deja log
 
     return (
-      <div className='container'>
+      <div className="container">
         <form onSubmit={this.handleSubmit}>
-          {this.renderInput("username", "Username", "text")}
-          {this.renderInput("password", "Password", "password")}
+          {this.renderInput('username', 'Username', 'text')}
+          {this.renderInput('password', 'Password', 'password')}
           <div>
-            <Link to='/login/forgot'>Forgot your password?</Link>
+            <Link to="/login/forgot">Forgot your password?</Link>
           </div>
-          {this.renderButton("Login")}
+          {this.renderButton('Login')}
         </form>
         <h4>Or</h4>
-        <Link to='/register'>Sign Up</Link>
+        <Link to="/register">Sign Up</Link>
       </div>
     );
   }
@@ -49,14 +53,17 @@ class LoginForm extends Form {
 
 function mapStateToProps({ user }) {
   return {
-    isAuth: user.isAuth,
+    isAuth: user.isAuth
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    attemptLogin: (user) => dispatch(attemptLogin(user)),
+    attemptLogin: user => dispatch(attemptLogin(user))
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginForm);
--- client/src/components/views/LoginForgot.jsx
+++ client/src/components/views/LoginForgot.jsx
@@ -1,18 +1,21 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
-import { connect } from "react-redux";
-import Joi from "joi-browser";
-import { Form } from "../shared";
-import { attemptSendResetPasswordLink } from "../../store/thunks/auth";
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Joi from 'joi-browser';
+import Form from '../common/form.jsx';
+import { attemptSendResetPasswordLink } from '../../store/thunks/auth';
 class LoginForgot extends Form {
   state = {
-    data: { email: "" },
+    data: { email: '' },
     errors: {},
-    submited: false,
+    submited: false
   };
 
   schema = {
-    email: Joi.string().email({ minDomainAtoms: 2 }).required().label("Email"),
+    email: Joi.string()
+      .email({ minDomainAtoms: 2 })
+      .required()
+      .label('Email')
   };
 
   doSubmit = async () => {
@@ -20,7 +23,7 @@ class LoginForgot extends Form {
     await this.props
       .attemptSendResetPasswordLink(data.email)
       .then(() => this.setState({ submited: true }))
-      .catch((error) => {
+      .catch(error => {
         if (error.response && error.response.status === 400) {
           const errors = { ...this.state.errors };
           errors.email = error.response.data.message;
@@ -30,25 +33,26 @@ class LoginForgot extends Form {
   };
 
   render() {
-    if (this.props.isAuth) return <Redirect to='/home' />; // ne peut pas se relog si deja log
+    if (this.props.isAuth) return <Redirect to="/home" />; // ne peut pas se relog si deja log
 
     return (
       <React.Fragment>
         {!this.state.submited && (
-          <div className='container'>
+          <div className="container">
             <p>We will send you a reset link on the following email :</p>
             <form onSubmit={this.handleSubmit}>
-              {this.renderInput("email", "Email", "text")}
-              {this.renderButton("SEND RESET LINK")}
+              {this.renderInput('email', 'Email', 'text')}
+              {this.renderButton('SEND RESET LINK')}
             </form>
           </div>
         )}
         {this.state.submited && (
-          <div className='container'>
+          <div className="container">
             <p>A reset link has been sent.</p>
             <p>
-              An reset link has been sent to :{this.state.data.email}. You have 12 hours to activate
-              your account. It can take up to 15 min to receive our email.
+              An reset link has been sent to :{this.state.data.email}. You have
+              12 hours to activate your account. It can take up to 15 min to
+              receive our email.
             </p>
           </div>
         )}
@@ -59,14 +63,18 @@ class LoginForgot extends Form {
 
 function mapStateToProps({ user }) {
   return {
-    isAuth: user.isAuth,
+    isAuth: user.isAuth
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    attemptSendResetPasswordLink: (email) => dispatch(attemptSendResetPasswordLink(email)),
+    attemptSendResetPasswordLink: email =>
+      dispatch(attemptSendResetPasswordLink(email))
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginForgot);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginForgot);
