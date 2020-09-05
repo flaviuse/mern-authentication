fe8b338272df0fa2d846c6fe2ad40634d409676b
Flavien Rebattet <40322270+flaviuse@users.noreply.github.com>
2020-09-05 17:32:32 +0200

    feat(logging) - from local file to DB

--- server/startup/logging.js
+++ server/startup/logging.js
@@ -1,23 +1,23 @@
 const winston = require("winston");
 // To Log on mongoDB database use :
-// require('winston-mongodb')
+require("winston-mongodb");
 require("express-async-errors");
 
-module.exports = function() {
+module.exports = function () {
   winston.handleExceptions(
     new winston.transports.Console({ colorize: true, prettyPrint: true }),
     new winston.transports.File({ filename: "uncaughtExceptions.log" })
   );
 
-  process.on("unhandledRejection", ex => {
+  process.on("unhandledRejection", (ex) => {
     throw ex;
   });
   // Local file
-  winston.add(winston.transports.File, { filename: "logfile.log" });
+  // winston.add(winston.transports.File, { filename: "logfile.log" });
 
   // Log on the MongoDB database
-  // winston.add(winston.transports.MongoDB, {
-  //   db: process.env.DB,
-  //   level: 'info'
-  // });
+  winston.add(winston.transports.MongoDB, {
+    db: process.env.DB,
+    level: "info",
+  });
 };
