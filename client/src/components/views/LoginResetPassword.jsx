156f104a74963752f9c20cc0603e6ea7e8186dc2
Flavien Rebattet <40322270+flaviuse@users.noreply.github.com>
2020-09-06 23:52:01 +0200

    fix (views) - styling and path import

--- client/src/components/views/LoginResetPassword.jsx
+++ client/src/components/views/LoginResetPassword.jsx
@@ -1,27 +1,24 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import Joi from 'joi-browser';
-import Form from '../common/form.jsx';
-import { attemptResetPassword } from '../../store/thunks/auth';
+import React from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import Joi from "joi-browser";
+import Form from "../shared/form.jsx";
+import { attemptResetPassword } from "../../store/thunks/auth";
 class LoginForgot extends Form {
   state = {
-    data: { password: '' },
-    errors: {}
+    data: { password: "" },
+    errors: {},
   };
 
   schema = {
-    password: Joi.string()
-      .min(5)
-      .max(255)
-      .required()
+    password: Joi.string().min(5).max(255).required(),
   };
 
   doSubmit = async () => {
     const { data } = this.state;
     const token = this.props.match.params.token;
     const password = data.password;
-    await this.props.attemptResetPassword(password, token).catch(error => {
+    await this.props.attemptResetPassword(password, token).catch((error) => {
       if (error.response && error.response.status === 400) {
         const errors = { ...this.state.errors };
         errors.password = error.response.data.message;
@@ -31,13 +28,13 @@ class LoginForgot extends Form {
   };
 
   render() {
-    if (this.props.isAuth) return <Redirect to="/home" />; // ne peut pas se relog si deja log
+    if (this.props.isAuth) return <Redirect to='/home' />; // ne peut pas se relog si deja log
     return (
-      <div className="container">
+      <div className='container'>
         <p>New password</p>
         <form onSubmit={this.handleSubmit}>
-          {this.renderInput('password', 'Password', 'password')}
-          {this.renderButton('RESET PASSWORD')}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("RESET PASSWORD")}
         </form>
       </div>
     );
@@ -46,18 +43,14 @@ class LoginForgot extends Form {
 
 function mapStateToProps({ user }) {
   return {
-    isAuth: user.isAuth
+    isAuth: user.isAuth,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    attemptResetPassword: (password, token) =>
-      dispatch(attemptResetPassword(password, token))
+    attemptResetPassword: (password, token) => dispatch(attemptResetPassword(password, token)),
   };
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(LoginForgot);
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForgot);
