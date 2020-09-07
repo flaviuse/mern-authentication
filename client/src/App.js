6a0fa9e3af2e4b159c1d31aa298788350faf7f8b
Flavien Rebattet <40322270+flaviuse@users.noreply.github.com>
2020-09-07 13:45:58 +0200

    fix(App) - styling / Import path

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Route, Redirect, Switch } from 'react-router-dom';
-import { ConnectedRouter } from 'connected-react-router';
-import { Provider } from 'react-redux';
-import { attemptGetUser } from './store/thunks/user';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Route, Redirect, Switch } from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
+import { Provider } from "react-redux";
+import { attemptGetUser } from "./store/thunks/user";
 
-import ConfirmPage from './components/pages/ConfirmPage.jsx';
-import Home from './components/pages/Home.jsx';
-import Profile from './components/pages/Profile.jsx';
-import NavBar from './components/pages/NavBar.jsx';
-import Login from './components/pages/Login.jsx';
-import LoginForgot from './components/pages/LoginForgot.jsx';
-import LoginResetPassword from './components/pages/LoginResetPassword.jsx';
-import Logout from './components/pages/Logout.jsx';
-import Register from './components/pages/Register.jsx';
-import ProtectedRoute from './components/common/protectedRoute.jsx';
+import ConfirmPage from "./components/views/ConfirmPage.jsx";
+import Home from "./components/views/Home.jsx";
+import Profile from "./components/views/Profile.jsx";
+import NavBar from "./components/views/NavBar.jsx";
+import Login from "./components/views/Login.jsx";
+import LoginForgot from "./components/views/LoginForgot.jsx";
+import LoginResetPassword from "./components/views/LoginResetPassword.jsx";
+import Logout from "./components/views/Logout.jsx";
+import Register from "./components/views/Register.jsx";
+import ProtectedRoute from "./components/shared/protectedRoute.jsx";
 
 class App extends Component {
   state = {
-    loading: true
+    loading: true,
   };
 
   componentDidMount() {
@@ -36,23 +36,16 @@ class App extends Component {
             <React.Fragment>
               <NavBar />
               <Switch>
-                <Route path="/home" exact component={Home} />
-                <ProtectedRoute path="/my-profile" exact component={Profile} />
-                <Route
-                  path="/account/confirm/:token"
-                  exact
-                  component={ConfirmPage}
-                />
-                <Route path="/register" exact component={Register} />
-                <Route path="/login" exact component={Login} />
-                <Route path="/login/forgot" exact component={LoginForgot} />
-                <Route
-                  path="/login/reset/:token"
-                  component={LoginResetPassword}
-                />
-                <ProtectedRoute path="/logout" exact component={Logout} />
-                <Redirect from="/" exact to="/home" />
-                <Redirect to="/home" />
+                <Route path='/home' exact component={Home} />
+                <ProtectedRoute path='/my-profile' exact component={Profile} />
+                <Route path='/account/confirm/:token' exact component={ConfirmPage} />
+                <Route path='/register' exact component={Register} />
+                <Route path='/login' exact component={Login} />
+                <Route path='/login/forgot' exact component={LoginForgot} />
+                <Route path='/login/reset/:token' component={LoginResetPassword} />
+                <ProtectedRoute path='/logout' exact component={Logout} />
+                <Redirect from='/' exact to='/home' />
+                <Redirect to='/home' />
               </Switch>
             </React.Fragment>
           </ConnectedRouter>
@@ -62,13 +55,10 @@ class App extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  attemptGetUser: () => dispatch(attemptGetUser())
+const mapDispatchToProps = (dispatch) => ({
+  attemptGetUser: () => dispatch(attemptGetUser()),
 });
 
 const mapStateToProps = null;
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
