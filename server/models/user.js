932cd434a2abce6d0ea0f083112cf5f7c8cbf02a
Flavien Rebattet <40322270+flaviuse@users.noreply.github.com>
2020-09-20 18:58:31 +0200

    fix (user model server) - validation rules

--- server/models/user.js
+++ server/models/user.js
@@ -70,7 +70,7 @@ const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
   const schema = Joi.object({
-    username: Joi.string().min(2).max(50).required(),
+    username: Joi.string().min(3).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(255).required(),
     isAdmin: Joi.boolean().required(),
@@ -81,7 +81,7 @@ function validateUser(user) {
 
 function validateLoginInput(input) {
   const schema = Joi.object({
-    username: Joi.string().min(3).max(255).required(),
+    username: Joi.string().min(3).max(50).required(),
     password: Joi.string().min(5).max(255).required(),
   });
 
@@ -90,7 +90,7 @@ function validateLoginInput(input) {
 
 function validateRegisterInput(input) {
   const schema = Joi.object({
-    username: Joi.string().min(3).max(255).required(),
+    username: Joi.string().min(3).max(50).required(),
     password: Joi.string().min(5).max(255).required(),
     email: Joi.string().min(5).max(255).required().email(),
   });
