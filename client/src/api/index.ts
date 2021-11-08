8c6ec35f936b4a60a351be245127a2de2a73f86c
Flavien Rebattet <40322270+flaviuse@users.noreply.github.com>
2021-11-08 22:43:38 +0100

    Added typing to http requests

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,15 +1,24 @@
 import { Credentials, User } from "src/store/actions/user";
 import http from "../services/httpService";
 
-const postLogin = (credentials: Credentials) => http.post("/auth/login", credentials);
+const postLogin = (credentials: Credentials) =>
+  http.post<{ user: User }>("/auth/login", credentials);
+
 const sendResetPasswordLink = (email: string) => http.post("/auth/login/forgot", { email });
+
 const resetPassword = (password: string, token: string) =>
-  http.post(`/auth/login/reset/${token}`, { password });
-const postLogout = () => http.post("/auth/logout");
-const postRegister = (user: User) => http.post("/auth/register", user);
-const getConfirmation = (token: string) => http.get(`/auth/confirmation/${token}`);
-const resendConfirmation = (email: string) => http.post("/auth/resend", { email });
-const resetRegister = (email: string) => http.post("/auth/register/reset", { email });
+  http.post<void>(`/auth/login/reset/${token}`, { password });
+
+const postLogout = () => http.post<void>("/auth/logout");
+
+const postRegister = (user: User) => http.post<void>("/auth/register", user);
+
+const getConfirmation = (token: string) => http.get<void>(`/auth/confirmation/${token}`);
+
+const resendConfirmation = (email: string) => http.post<void>("/auth/resend", { email });
+
+const resetRegister = (email: string) => http.post<void>("/auth/register/reset", { email });
+
 const getUser = () => http.get<{ user: User }>("/user");
 
 export {
