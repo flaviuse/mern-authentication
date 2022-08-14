74bded0cd2290e61d0fa857449eeb76dcd6d9559
Flavien Rebattet <40322270+flaviuse@users.noreply.github.com>
2022-08-14 20:50:51 +0200

    Increase rate limiter

--- server/startup/rate-limit.ts
+++ server/startup/rate-limit.ts
@@ -3,7 +3,7 @@ import { Express } from "express";
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 5,
+  max: 20,
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
